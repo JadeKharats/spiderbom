c3a59e50d84db99d17e001fb8a121d712130e17c
Jade Kharats <jade.kharats@gmail.com>
2020-09-17 20:32:01 +0200

    fix

--- src/models/UserInfos.cr
+++ src/models/UserInfos.cr
@@ -1,11 +1,13 @@
 class UserInfos
   include JSON::Serializable
 
+  property id : Int64
   property name : String
   property email : String
   property avatar_url : String
 
   def initialize
+    @id = 0
     @name = "Undefined name"
     @email = "Undefined email"
     @avatar_url = "Undefined avatar_url"
